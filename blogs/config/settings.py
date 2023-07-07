408c03bc1f40b3af0dbf34fd7cb5bc8c62d8d161
kiyongee2 <kiyongee2@gmail.com>
2023-07-07 19:02:20 +0900

    debug

--- blogs/config/settings.py
+++ blogs/config/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q=w53h7k)+zhk$smk0rb133rvi^3p-a7y-3=%^aizx(vcim)u7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
